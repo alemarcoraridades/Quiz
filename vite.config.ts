1c13c413d529d65021264ad6b0eb9d445ad13b9d
alemarcoraridades <alemarco.raridades@gmail.com>
2025-06-24 07:34:32 -0300

    Fix .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_');
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
       outDir: 'docs'
     },
     resolve: {
-      alias: { '@': path.resolve(__dirname, './src') }
+      alias: { '@': resolve(__dirname, './src') }
     },
     server: {
       port: 3000,
